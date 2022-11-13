fb0fef6ecaca985c627efc3ea772e34bc266a684
EC2 Default User <ec2-user@ip-172-31-92-227.ec2.internal>
2022-11-13 12:35:56 +0000

    11/13

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,24 +3,24 @@ class UsersController < ApplicationController
      @user = User.find(params[:id])
      @comics=@user.comics.page(params[:page]).reverse_order
   end
-  
+
   def edit
     @user = User.find(params[:id])
     unless @user.id == current_user.id
        redirect_to comics_path
     end
   end
-  
+
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user.id)
   end
-  
+
   private
 
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
-  
+
 end
